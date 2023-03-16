fd553c84a3b5450e2c27256cd0928c369566391a
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-03-16 16:40:11 +0100

    "Deploy"

---
+++ out/_next/static/a5IslagahcJ145IDnQyO5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/133-89137b6ed06c43b7.js","static/chunks/pages/index-559ed9a341a91edd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
