3f2ad02922d6c78fd46ac70bdb84be1be522b3a8
bouzia_n <bouzianenassim17102002@gmail.com>
2024-12-03 20:10:16 +0100

    Updates

--- _next/static/UBJkLEYrWt824TXlf0agn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/133-89137b6ed06c43b7.js","static/chunks/pages/index-559ed9a341a91edd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
